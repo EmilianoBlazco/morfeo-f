25446cb27f9c3ef97a3d45cc1d0e6a438c57b9af
Emiliano Blazco <emiliano.blazco@fceqyn.unam.edu.ar>
2024-10-13 12:01:20 -0300

    Fix(1/autenticacion(project_structure)): Prueba de codigo para solucionar problemas de produccion en vercel (v.2)

---
+++ src/components/self/views/app/dashboard.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import withLayout from "@/lib/withLayout";
+
+const DashboardComponent = () => {
+
+    return (
+        <h1>Dashboard</h1>
+    )
+
+}
+
+export default withLayout(DashboardComponent);
