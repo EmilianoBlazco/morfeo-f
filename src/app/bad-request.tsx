7ab2c13dd4cc18fdfa259eb5c145eb2db53f5dd1
Emiliano Blazco <emiliano.blazco@fceqyn.unam.edu.ar>
2024-10-30 12:54:40 -0300

    Feat(1/all): Se integro el manejo de errores mediante un hook personalizado.
    Explicacion detallada:
    - Se creo el hook useHandleError.ts
    - Se asociaron las pantallas de error finales para su redireccion (Version preliminar).

---
+++ src/app/bad-request.tsx
@@ -0,0 +1,12 @@
+export const BadRequest = () => {
+    return (
+        <div>
+            <h1>
+                Solicitud no procesada
+            </h1>
+        </div>
+    );
+
+}
+
+export default BadRequest;
---
+++ src/app/internal-server-error.tsx
@@ -0,0 +1,12 @@
+export const InternalServerError = () => {
+    return (
+        <div>
+            <h1>
+                No se pudo completar la peticion
+            </h1>
+        </div>
+    );
+
+}
+
+export default InternalServerError;
