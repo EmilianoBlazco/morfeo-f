9a2a9b760ddf5368d29be2ebc5aab53c6e730fb7
CristaldoYonathan <ariel.cristaldo@fceqyn.unam.edu.ar>
2024-11-01 14:18:25 -0300

    style(1/autenticacion(landing_config)): Se modificó la posición de la imágen en login.

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -8,6 +8,13 @@ import Portada from "../../../../public/images/Portada.png";
 const LoginPage = () => {
     return (
         <div className="w-full min-h-screen lg:grid lg:grid-cols-2">
+            <div className="flex items-center justify-center py-12 lg:overflow-hidden lg:max-h-screen">
+                <AnimatedWrapper initialX={50} exitX={50} duration={1}>
+                    <div className="w-full max-h-full lg:max-h-screen overflow-y-auto mt-10 lg:overflow-y-auto">
+                        <Login/>
+                    </div>
+                </AnimatedWrapper>
+            </div>
             <AnimatedWrapper initialX={-50} exitX={50} duration={1}>
                 <div className="relative hidden bg-muted lg:block h-full">
                     <Image
@@ -22,13 +29,6 @@ const LoginPage = () => {
                     </h1>
                 </div>
             </AnimatedWrapper>
-            <div className="flex items-center justify-center py-12 lg:overflow-hidden lg:max-h-screen">
-                <AnimatedWrapper initialX={50} exitX={50} duration={1}>
-                    <div className="w-full max-h-full lg:max-h-screen overflow-y-auto mt-10 lg:overflow-y-auto">
-                        <Login />
-                    </div>
-                </AnimatedWrapper>
-            </div>
         </div>
     );
 }
