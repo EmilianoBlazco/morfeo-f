154c8e511a41cfd38fc4218ec92e1d7807e7b77b
CristaldoYonathan <ariel.cristaldo@fceqyn.unam.edu.ar>
2024-11-01 14:14:01 -0300

    fix(1/autenticacion(landing_config)): Se corrigió errores de visualización de la imagen de portada de Morfeo.
    Explicación detallada:
    - Se modificó la manera en la que se acceden a las imágenes en Login y Register.

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,33 +3,34 @@
 import Image from "next/image";
 import Login from "@components/self/views/auth/login";
 import AnimatedWrapper from "@components/self/animations/enter_from_side";
+import Portada from "../../../../public/images/Portada.png";
 
 const LoginPage = () => {
     return (
-        <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
-            <div className="flex items-center justify-center py-12">
-                <AnimatedWrapper initialX={-50} exitX={50} duration={1}>
-                    <Login />
-                </AnimatedWrapper>
-            </div>
-            <AnimatedWrapper initialX={50} exitX={50} duration={1}>
-                <div className="flex items-center justify-center relative">
+        <div className="w-full min-h-screen lg:grid lg:grid-cols-2">
+            <AnimatedWrapper initialX={-50} exitX={50} duration={1}>
+                <div className="relative hidden bg-muted lg:block h-full">
                     <Image
-                        src="/images/Portada.png"
+                        src={Portada}
                         alt="Imagen de portada"
-                        width={1920}
-                        height={1080}
-                        className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+                        layout="fill"
+                        objectFit="cover"
+                        className="dark:brightness-[0.2] dark:grayscale"
                     />
-
                     <h1 className="absolute top-24 left-1/2 transform -translate-x-1/2 text-gray-500 font-bold text-4xl md:text-6xl lg:text-7xl whitespace-nowrap">
                         Morfeo S. A.
                     </h1>
                 </div>
             </AnimatedWrapper>
+            <div className="flex items-center justify-center py-12 lg:overflow-hidden lg:max-h-screen">
+                <AnimatedWrapper initialX={50} exitX={50} duration={1}>
+                    <div className="w-full max-h-full lg:max-h-screen overflow-y-auto mt-10 lg:overflow-y-auto">
+                        <Login />
+                    </div>
+                </AnimatedWrapper>
+            </div>
         </div>
-    )
-        ;
+    );
 }
 
 export default LoginPage;
